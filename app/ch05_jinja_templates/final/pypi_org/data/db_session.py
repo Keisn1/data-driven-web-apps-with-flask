015e9fef3675c88e68c1a3b013817ff879dc30bb
KayPro <kaypro@Kays-Mac-mini.local>
2023-06-14 15:15:59 +0200

    Last session

--- app/ch05_jinja_templates/final/pypi_org/data/db_session.py
+++ app/ch05_jinja_templates/final/pypi_org/data/db_session.py
@@ -1,15 +1,16 @@
 import sqlalchemy as sa
 import sqlalchemy.orm as orm
+from sqlalchemy.orm import Session
 
 from data.modelbase import SqlAlchemyBase
 
-factory = None
+__factory = None
 
 
 def global_init(db_file: str):
-    global factory
+    global __factory
 
-    if factory:
+    if __factory:
         return
 
     if not db_file or not db_file.strip():
@@ -20,8 +21,13 @@ def global_init(db_file: str):
     engine = sa.create_engine(conn_str, echo=False)
 
     # creates a callable, which create session at call
-    factory = orm.sessionmaker(bind=engine)
+    __factory = orm.sessionmaker(bind=engine)
 
     # noinspection PyUnresolvedReferences
     import data.__all_models
     SqlAlchemyBase.metadata.create_all(engine)
+
+
+def create_session() -> Session:
+    global __factory
+    return __factory()
